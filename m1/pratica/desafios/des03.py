f68b90eb397180cd36c33791340fe610b9861e92
Leandro Galvão <150615368+galvandro@users.noreply.github.com>
2024-04-03 09:49:22 -0300

    Update des03.py

--- m1/pratica/desafios/des03.py
+++ m1/pratica/desafios/des03.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um numero...'))
-n2 = int(input('Digite outro numero...'))
+n1 = int(input('Digite um número… '))
+n2 = int(input('Digite outro número… '))
 
-print(f'Muito bem!! A soma entre {n1} e {n2}, e {n1 + n2}.')
+print(f'Muito bem!! A soma entre {n1} e {n2}, é {n1 + n2}.')
