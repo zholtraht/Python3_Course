6f1ccbb6b38291507930859911a33b14438768c0
Leandro Galvão <leandrogalvao.txt@gmail.com>
2024-04-03 09:22:11 -0300

    create "des" files

---
+++ m1/pratica/desafios/des02.py
@@ -0,0 +1,6 @@
+n = str(input('Digite o seu nome...'))
+d = int(input('Dia do nascimento...'))
+m = str(input('Mes do nascimento...'))
+a = int(input('Ano do nascimento...'))
+
+print(f'Muito bem, {n}!! Voce nasceu no dia {d}, de {m}, de {a}. Correto?.')
---
+++ m1/pratica/desafios/des03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um numero...'))
+n2 = int(input('Digite outro numero...'))
+
+print(f'Muito bem!! A soma entre {n1} e {n2}, e {n1 + n2}.')
