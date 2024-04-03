13b047424ce11b3ed2d6843e76f21c9578ccbcb7
Leandro Galvão <150615368+galvandro@users.noreply.github.com>
2024-04-03 09:47:41 -0300

    Update des02.py

--- m1/pratica/desafios/des02.py
+++ m1/pratica/desafios/des02.py
@@ -1,6 +1,6 @@
-n = str(input('Digite o seu nome...'))
-d = int(input('Dia do nascimento...'))
-m = str(input('Mes do nascimento...'))
-a = int(input('Ano do nascimento...'))
+n = str(input('Digite o seu nome… '))
+d = int(input('Dia do nascimento… '))
+m = str(input('Mês do nascimento… '))
+a = int(input('Ano do nascimento… '))
 
-print(f'Muito bem, {n}!! Voce nasceu no dia {d}, de {m}, de {a}. Correto?.')
+print(f'Muito bem, {n}!! Você nasceu no dia {d}, de {m}, de {a}. Correto?.')
