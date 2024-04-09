434c2b4c5e56a2376d7c6c3ced3052435febcf2c
Leandro Galvão <leandrogalvao.txt@gmail.com>
2024-04-09 09:07:09 -0300

    create new des files

---
+++ m1/pratica/desafios/des005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero...'))
+a = (n - 1)
+s = (n + 1)
+
+print(f'''Vejamos...voce digitou {n}, seu sucessor e {s}, e seu antecessor e {a}.''')
---
+++ m1/pratica/desafios/des006.py
@@ -0,0 +1,11 @@
+n = int(input('Digite um numero...'))
+d = (n * 2)
+t = (n * 3)
+r = n ** (1/2)
+
+print(f'''
+Vejamos...voce digitou {n}.
+Seu dobro e {d}
+seu triplo e {t}
+sua raiz quadrada e {r:.1f}
+''')
