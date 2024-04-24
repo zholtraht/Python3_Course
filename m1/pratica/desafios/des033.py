46eaa16b135d98b161cbb8e8c093afad247005ff
Leandro Galvão <leandrogalvao.txt@gmail.com>
2024-04-24 10:42:18 -0300

    Update des033.py

--- m1/pratica/desafios/des033.py
+++ m1/pratica/desafios/des033.py
@@ -2,3 +2,21 @@ n1 = int(input('Digite um numero... '))
 n2 = int(input('Digite outro numero... '))
 n3 = int(input('Outro numero... '))
 
+if n1 < n2 and n1 < n3:
+    me = n1
+if n2 < n1 and n2 < n3:
+    me = n2
+if n3 < n1 and n3 < n2:
+    me = n3
+
+if n1 > n2 and n1 > n3:
+    ma = n1
+if n2 > n1 and n2 > n3:
+    ma = n2
+if n3 > n1 and n3 > n2:
+    ma = n3
+
+print(f'''
+MAIOR: {ma}
+MENOR: {me}
+''')
