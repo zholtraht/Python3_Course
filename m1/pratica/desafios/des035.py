e7ee95fcb9c086acc153734f6153538ea260d602
Leandro Galvão <leandrogalvao.txt@gmail.com>
2024-04-24 10:42:26 -0300

    Update des035.py

--- m1/pratica/desafios/des035.py
+++ m1/pratica/desafios/des035.py
@@ -1,3 +1,12 @@
-r1 = 
-r2 =
-r3 =
+r1 = float(input('Primeiro segmento... ')) 
+r2 = float(input('Seundo segmento... '))
+r3 = float(input('Terceiro segmento... '))
+
+c1 = (r2 + r3)
+c2 = (r1 + r3)
+c3 = (r1 + r2)
+
+if r1 < c1 and r2 < c2 and r3 < c3:
+    print('''Um triangulo PODE ser formado... ''')
+else:
+    print('''Um triangulo NAO PODE ser formado... ''')
