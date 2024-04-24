b1db5fc4cf888606f8a4ef3c84538638d5707f36
Leandro Galvão <leandrogalvao.txt@gmail.com>
2024-04-24 10:42:10 -0300

    Update des032.py

--- m1/pratica/desafios/des032.py
+++ m1/pratica/desafios/des032.py
@@ -1,7 +1,9 @@
 a = int(input('Em que ano estamos?... '))
 cb = a % 4
+cb1 = a % 100
+cb2 = a % 400
 
-if cb == 0:
+if cb == 0 and cb1 != 0 or cb2 == 0:
     print(f'''BISSEXTO''')
 else:
     print(f'''NAO BISSEXTO''')
