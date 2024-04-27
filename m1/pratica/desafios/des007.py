e111cfe41a558d3241d2198c472a2102bfcf85b9
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:36 -0300

    Update des007.py

--- m1/pratica/desafios/des007.py
+++ m1/pratica/desafios/des007.py
@@ -1,5 +1,15 @@
-n1 = float(input('Digite uma nota...'))
-n2 = float(input('Digite outra nota...'))
+#Crie um um programa que:
+#Leia as duas notas de um aluno.
+#Calcule e mostre a sua média.
+
+n1 = float(input("Uma nota... "))
+n2 = float(input("Outra nota... "))
 m = (n1 + n2) / 2
 
-print(f'''Vejamos...suas notas foram {n1:.1f} e {n2:.1f}, logo sua media e {m:.1f}''')
+print(f"""
+Vejamos...
+
+Suas notas foram {n1:.1f} e {n2:.1f}.
+
+Sua média é {m:.1f}.
+""")
