37bce75f00d5a9034b664d3dffae91c8a2d465a4
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:29 -0300

    Update des005.py

--- m1/pratica/desafios/des005.py
+++ m1/pratica/desafios/des005.py
@@ -1,5 +1,15 @@
-n = int(input('Digite um numero...'))
+#Faça um programa que:
+#Leia um número Inteiro.
+#Mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input("Um número... "))
 a = (n - 1)
 s = (n + 1)
 
-print(f'''Vejamos...voce digitou {n}, seu sucessor e {s}, e seu antecessor e {a}.''')
+print(f"""
+Vejamos…
+
+você digitou {n}.
+
+Seu sucessor é {s}, e seu antecessor é {a}.
+""")
