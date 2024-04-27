0d0ef72fd33c9923bc4a5ae50901da2a1fff578b
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:56 -0300

    Update des013.py

--- m1/pratica/desafios/des013.py
+++ m1/pratica/desafios/des013.py
@@ -1,6 +1,12 @@
-s = float(input('Salario atual de um funcionario... R$'))
+#Crie um programa que:
+#Leia o salário de um funcionário.
+#Mostre seu novo salário, com 15% de aumento.
+
+s = float(input("Salário atual de um funcionário... R$"))
 sa = s + (s * 15 / 100)
 
-print(f'''
-O salario de um funcionario que ganha R${s:.2f}, com 15% de aumento passa a ser {sa:.2f}
-''')
+print(f"""
+Vejamos...
+O funcionário que ganha R${s:.2f}.
+Com 15% de aumento passa a ganhar R${sa:.2f}.
+""")
