cc9cb294cbb6ed874cdd8e9d18d08ab0084f1cac
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:38:02 -0300

    Update des015.py

--- m1/pratica/desafios/des015.py
+++ m1/pratica/desafios/des015.py
@@ -1,7 +1,18 @@
-qd = int(input('Quatos dias alugados?... '))
-qk = float(input('Quantos km rodados?... '))
+#Crie um programa que:
+#Pergunte a quantidade de Km percorridos por um carro alugado.
+#Perunte a quantidade de dias pelos quais ele foi alugado.
+#Calcule o preço a pagar.
+#Considere que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+qd = int(input("""Dias alugados?... """))
+qk = float(input("""Km's rodados?... """))
 v = qd * 60 + qk * 0.15
 
-print(f'''
-O total a pagar e R${v:.2f}
-''')
+print(f"""
+vejamos...
+
+Você alugou por {qd} dias.
+rodou {qk:.1f}km.
+
+O total a pagar é R${v:.2f}.
+""")
