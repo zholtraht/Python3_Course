a3af67223bf85604ada8772e1cddf1ba0a023bb9
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:23 -0300

    Update des003.py

--- m1/pratica/desafios/des003.py
+++ m1/pratica/desafios/des003.py
@@ -1,4 +1,13 @@
-n1 = int(input('Digite um número… '))
-n2 = int(input('Digite outro número… '))
+#Crie um programa que:
+#Leia dois números.
+#Mostre a soma entre eles.
 
-print(f'Muito bem!! A soma entre {n1} e {n2}, é {n1 + n2}.')
+n1 = int(input("Um número… "))
+n2 = int(input("Outro número… "))
+s = (n1 + n2)
+
+print(f"""
+Muito bem!!
+
+A soma entre {n1} e {n2}, é {s}.
+""")
