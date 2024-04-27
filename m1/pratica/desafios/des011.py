9a45e69942f921009e9319785facbf4d3aec7a2d
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:50 -0300

    Update des011.py

--- m1/pratica/desafios/des011.py
+++ m1/pratica/desafios/des011.py
@@ -1,10 +1,17 @@
-lp = float(input('Largura da parede...'))
-ap = float(input('Altura da parede...'))
-a = lp * ap
-t = a / 2
+#Crie um programa que:
+#Leia a largura e a altura de uma parede em metros.
+#Calcule a sua área e a quantidade de tinta necessária para pintá-la.
+#Considere que cada litro de tinta pinta uma área de 2 metros quadrados.
 
-print(f'''
-Vejamos sua parede possui dimesao {lp:.0f}x{ap:.0f}, e area {a:.1f}m²
+lp = float(input("Largura da parede... "))
+ap = float(input("Altura da parede... "))
+a = (lp * ap)
+t = (a / 2)
 
-Para pitar essa parede, precisas de {t:.1f}l de tinta
-''')
+print(f"""
+Vejamos...
+
+Sua parede possui dimensão {lp:.0f}x{ap:.0f}, e área {a:.1f}m².
+
+Para pitar essa parede, precisas de {t:.1f}l de tinta.
+""")
