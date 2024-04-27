6cb619b98c3517e9b4dc3099b5e3b45b9fdab8ec
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:43 -0300

    Update des009.py

--- m1/pratica/desafios/des009.py
+++ m1/pratica/desafios/des009.py
@@ -1,18 +1,22 @@
-n = int(input('Digite um numero...'))
+#Crie um programa que:
+#Leia um número Inteiro.
+#Mostre na tela a sua tabuada.
 
-print(f'''
- Muito bem!!
+n = int(input("Um número... "))
 
- tabuada de {n}:
+print(f"""
+TABUADA DE {n}:
 
- {n} x  1 = {n * 1} 
- {n} x  2 = {n * 2}
- {n} x  3 = {n * 3}
- {n} x  4 = {n * 4}
- {n} x  5 = {n * 5}
- {n} x  6 = {n * 6}
- {n} x  7 = {n * 7}
- {n} x  8 = {n * 8}
- {n} x  9 = {n * 9}
- {n} x 10 = {n * 10} 
-''')
+{'=' * 13}
+{n} x 1  = {n * 1} 
+{n} x 2  = {n * 2}
+{n} x 3  = {n * 3}
+{n} x 4  = {n * 4}
+{n} x 5  = {n * 5}
+{n} x 6  = {n * 6}
+{n} x 7  = {n * 7}
+{n} x 8  = {n * 8}
+{n} x 9  = {n * 9}
+{n} x 10 = {n * 10}
+{'=' * 13}
+""")
