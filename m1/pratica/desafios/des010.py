fbe7f915e3f3ba6245ded876f2e5651f7625cdc1
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:46 -0300

    Update des010.py

--- m1/pratica/desafios/des010.py
+++ m1/pratica/desafios/des010.py
@@ -1,7 +1,15 @@
-v = float(input('Quanto dinheiro voce tem na carteira?...'))
-d = v / 5.01
+#Crie um programa que:
+#Leia quanto dinheiro uma pessoa tem na carteira.
+#Mostre quantos dólares ela pode comprar. 
+#Cotação do dia: U$5.01
 
-print(f'''
-Vejamos...voce possui R${v:.2f}.
-Com essa quantia voce pode comprar U${d:.2f}
-''')
+r = float(input("Quantidade de dinheiro na carteira... "))
+d = r / 5.01
+
+print(f"""
+Vejamos...
+
+Você possui R${r:.2f}.
+
+Com essa quantia você pode comprar U${d:.2f}.
+""")
