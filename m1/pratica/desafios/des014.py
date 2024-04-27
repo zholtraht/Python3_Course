31f0f3650e34cbebd01148e17ebb42345993cb85
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-27 20:37:59 -0300

    Update des014.py

--- m1/pratica/desafios/des014.py
+++ m1/pratica/desafios/des014.py
@@ -1,6 +1,11 @@
-c = float(input('Informe um valor em ⁰C... '))
+#Crie um programa que:
+#Leia uma temperatura em graus Celsius.
+#Converta para graus Fahrenheit.
+
+c = float(input("Informe um valor em ⁰C... "))
 f = (c * 9/5) + 32
 
-print(f'''
-Voce digitou {c:.1f}⁰C, essa temperatura convertida em fahrenheit e {f:.1f}⁰F
-''')
+print(f"""
+Você digitou {c:.1f}⁰C.
+Essa temperatura convertida em fahrenheit é {f:.1f}⁰F.
+""")
