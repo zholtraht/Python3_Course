a1a9d98d3e42e9386b7ca46416604905145cafc6
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:45 -0300

    Update des022.py

--- m1/pratica/desafios/des022.py
+++ m1/pratica/desafios/des022.py
@@ -1,8 +1,20 @@
-n = str(input('Seu nome completo...')).strip()
+#Crie um programa que:
+#Leia o nome completo de uma pessoa.
+#Exiba o nome com todas as letras maiúsculas.
+#Exiba o nome com todas as letras minúsculas.
+#Mostre quantas letras ao todo (sem considerar espaços).
+#Mostre quantas letras tem o primeiro nome.
 
-print(f'''
+n = str(input("Nome completo... ")).strip()
+
+print(f"""
 Muito bem!! Vejamos...
-Seu nome em maiusculas es: {n.upper()}
-Seu nome me minusculas es: {n.lower()}
-Seu nome possui {len(n) - n.count(' ')} letras
-Seu primeiro nome possui {n.find(' ')} letras''')
+
+Seu nome em maiúsculas é: {n.upper()}.
+{'=' * 5}
+Seu nome me minúsculas é: {n.lower()}.
+{'=' * 5}
+Seu nome possui {len(n) - n.count(' ')} letras.
+{'=' * 5}
+Seu primeiro nome possui {n.find(' ')} letras.
+""")
