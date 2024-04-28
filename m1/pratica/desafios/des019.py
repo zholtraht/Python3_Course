90ab35f50db8f8aa1f751d2390f963942fc00855
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:38 -0300

    Update des019.py

--- m1/pratica/desafios/des019.py
+++ m1/pratica/desafios/des019.py
@@ -1,12 +1,20 @@
+#"Um professor vai sortear um dos seus quatro alunos para apagar o quadro"
+
+#Crie um programa que:
+#Leia o nome dos alunos.
+#Escreva na tela o nome do escolhido.
+
 from random import choice
 
-pa = str(input('Primeiro aluno...'))
-sa = str(input('Segundo aluno...'))
-ta = str(input('Terceiro aluno...'))
-qa = str(input('Quarto aluno...'))
+pa = str(input("Primeiro aluno... ")).upper()
+sa = str(input("Segundo aluno... ")).upper()
+ta = str(input("Terceiro aluno... ")).upper()
+qa = str(input("Quarto aluno... ")).upper()
+
 l = [pa, sa, ta, qa]
+
 rs = choice(l)
 
-print(f'''
-O aluno escolhido foi {rs}
-''')
+print(f"""
+O aluno escolhido foi {rs}.
+""")
