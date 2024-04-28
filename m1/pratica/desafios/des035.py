1cfd11bcf1bb9503ddcd9f75c5b105d7c501330e
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:41:07 -0300

    Update des035.py

--- m1/pratica/desafios/des035.py
+++ m1/pratica/desafios/des035.py
@@ -1,12 +1,20 @@
-r1 = float(input('Primeiro segmento... ')) 
-r2 = float(input('Seundo segmento... '))
-r3 = float(input('Terceiro segmento... '))
+#Crie um programa que:
+#Leia o comprimento de três retas.
+#Diga ao usuário se elas podem ou não formar um triângulo.
+
+r1 = float(input("Primeiro segmento... ")) 
+r2 = float(input("Seundo segmento... "))
+r3 = float(input("Terceiro segmento... "))
 
 c1 = (r2 + r3)
 c2 = (r1 + r3)
 c3 = (r1 + r2)
 
 if r1 < c1 and r2 < c2 and r3 < c3:
-    print('''Um triangulo PODE ser formado... ''')
+    print("""
+    Um triângulo PODE ser formado...
+    """)
 else:
-    print('''Um triangulo NAO PODE ser formado... ''')
+    print("""
+    Um triângulo NÃO PODE ser formado...
+    """)
