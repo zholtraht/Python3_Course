84ee48e94bc61b475ba4f7f11a0f4aa5b87b1708
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 12:44:15 -0300

    Update des012.py

--- m1/pratica/desafios/des012.py
+++ m1/pratica/desafios/des012.py
@@ -2,7 +2,7 @@
 #Leia o preço de um produto.
 #Mostre seu novo preço, com 5% de desconto.
 
-vp = float(input('Valor do produto... R$'))
+vp = float(input("Valor do produto... R$"))
 vd = (vp * 5 / 100)
 pd = (vp - vd)
 
