9d88bf868c138b6eceac7ab72895a9de7ecdd6d5
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:41 -0300

    Update des020.py

--- m1/pratica/desafios/des020.py
+++ m1/pratica/desafios/des020.py
@@ -1,12 +1,25 @@
+#"Um professor quer sortear a ordem de apresentação de trabalhos dos alunos."
+
+#Crie um programa que:
+#Leia o nome dos quatro alunos.
+#Mostre a ordem sorteada.
+
 from random import shuffle
 
-pa = str(input('Primeiro aluno...'))
-sa = str(input('Segudo aluno...'))
-ta = str(input('Terceiro aluno...'))
-qa = str(input('Quarto aluno...'))
+pa = str(input("Primeiro aluno... "))
+sa = str(input("Segudo aluno... "))
+ta = str(input("Terceiro aluno... "))
+qa = str(input("Quarto aluno... "))
+
 l = [pa, sa, ta, qa]
+
 rs = shuffle(l)
 
-print(f'''
-A ordem de apresentacao sera: {l}
-''')
+print(f"""
+A ordem de apresentação será:
+
+1. {l[0]}.
+2. {l[1]}.
+3. {l[2]}.
+4. {l[3]}.
+""")
