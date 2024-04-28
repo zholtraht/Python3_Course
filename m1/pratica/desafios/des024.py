734d13b400de06c4d4ff52e06162ed6dfa9756f2
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:48 -0300

    Update des024.py

--- m1/pratica/desafios/des024.py
+++ m1/pratica/desafios/des024.py
@@ -1,3 +1,7 @@
-c = str(input('Em que cidade voce nasceu?...')).strip()
+#Crie um programa que:
+#Leia o nome de uma cidade.
+#Diga se ela começa ou não com o nome “SANTO”.
+
+c = str(input("Em que cidade você nasceu?... ")).strip()
 
 print(c[0:5].upper() == 'SANTO')
