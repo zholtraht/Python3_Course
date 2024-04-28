793857f008117c005be81d5ce99ccbd77b1932a3
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:54 -0300

    Update des028.py

--- m1/pratica/desafios/des028.py
+++ m1/pratica/desafios/des028.py
@@ -1,16 +1,21 @@
+#Crie um programa que:
+#Faça o computador “pensar” em um número inteiro entre 0 e 5.
+#Peça para o usuário tentar descobrir qual foi o número escolhido.
+#Escrever na tela se o usuário venceu ou perdeu.
+
 from random import randint
 
 n = randint(0, 5)
 
-print('''
-Acabo de pensar em um numero inteiro de 0 a 5
+print("""
+Acabo de pensar em um número inteiro entre 0 e 5.
+{'=' * 10}
+Tente adivinhar o número escolhido:
+""")
 
-Tentes adivinhar o numero escolhido:
-''')
-
-ne = int(input('Seu palpite... '))
+ne = int(input("Seu palpite... "))
 
 if ne == n:
-    print('ACERTOU!! Acabas de me vencer')
+    print("ACERTOU!! Acabas de me vencer.")
 else:
-    print(f'ERROU!! Minha esolha foi {n}')
+    print(f"ERROU!! Minha escolha foi {n}.")
