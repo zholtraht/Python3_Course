9fa7eddf60d0e38fc6ec888926b342f9a7cd4778
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:47 -0300

    Update des023.py

--- m1/pratica/desafios/des023.py
+++ m1/pratica/desafios/des023.py
@@ -1,13 +1,21 @@
-n = int(input('Digite um numero...'))
+#Crie um programa que:
+#Leia um número de 0 a 9999.
+#Mostre na tela cada um dos dígitos separados.
+
+n = int(input("Um número... "))
 u = (n // 1 % 10)
 d = (n // 10 % 10)
 c = (n // 100 % 10)
 m = (n // 1000 % 10)
 
-print(f'''
-Vejamos... Voce digitou {n}
-Unidade: {u}
-Dezena: {d}
-Centena: {c}
-Milhar: {m}
-''')
+print(f"""
+Vejamos... Você digitou {n}.
+
+Unidade: {u}.
+{'=' * 5}
+Dezena: {d}.
+{'=' * 5}
+Centena: {c}.
+{'=' * 5}
+Milhar: {m}.
+""")
