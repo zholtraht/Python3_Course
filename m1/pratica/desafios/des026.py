f47693c1431f680384867febbeb63cc8b00262bf
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:51 -0300

    Update des026.py

--- m1/pratica/desafios/des026.py
+++ m1/pratica/desafios/des026.py
@@ -1,8 +1,17 @@
-f = str(input('Digite uma frase...')).strip().lower()
+#Crie um programa que:
+#Leia uma frase pelo teclado.
+#Mostre quantas vezes aparece a letra “A”.
+#Mostre em que posição a letra 'A' aparece a primeira vez.
+#Mostre em que posição a letra 'A' aparece a última vez.
 
-print(f'''
+f = str(input("Uma frase... ")).strip().lower()
+
+print(f"""
 Vejamos... "{f.title()}"
+
 A letra A aparece {f.count('a')} vezes na frase.
-A primeira letra A apareceu na posicao {f.find('a')+1}.
-A ultima letra A apareceu na posicao {f.rfind('a')+1}.
-''')
+{'=' * 5}
+A primeira letra A apareceu na posição {f.find('a')+1}.
+{'=' * 5}
+A última letra A apareceu na posição {f.rfind('a')+1}.
+""")
