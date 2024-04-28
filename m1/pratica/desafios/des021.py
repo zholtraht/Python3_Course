523e2ca76b63997d45210b3b42937b01bfee0085
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:43 -0300

    Update des021.py

--- m1/pratica/desafios/des021.py
+++ m1/pratica/desafios/des021.py
@@ -1,6 +1,12 @@
+#Crie um programa que:
+#Abra e reproduza o áudio de um arquivo MP3.
+
 import pygame
 
 pygame.init()
+
 pygame.mixer.music.load('PalmTree.mp3')
+
 pygame.mixer.music.play()
+
 pygame.event.wait()
