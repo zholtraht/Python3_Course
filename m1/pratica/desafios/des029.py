7b9dc81378c12f6d3d6759d6d3af53a6608ac823
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:40:56 -0300

    Update des029.py

--- m1/pratica/desafios/des029.py
+++ m1/pratica/desafios/des029.py
@@ -1,11 +1,23 @@
-vc = float(input('Velocidade do carro... '))
+#Crie um programa que:
+#Leia a velocidade de um carro.
+#Ultrapassando 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+#Considere que a multa vai custar R$7,00 por cada Km acima do limite.
+
+vc = float(input("Velocidade do carro... "))
 vm = (vc - 80) * 7
 
 if vc > 80:
-    print(f'''Multado!!
-voce estava a {vc - 80}km acima da velocidade permitida
-Sera multado em R${vm:.2f}''')
-    
+    print(f"""
+    MULTADO!!!
+
+    Você estava a {vc - 80}km acima da velocidade permitida.
+
+    Será multado em R${vm:.2f}
+    """)
+
 else:
-    print('''
-Tudo em ordem... ''')
+    print("""
+    EM ORDEM...
+
+    Conduza com cuidado!!
+    """)
