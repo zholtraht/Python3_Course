dd758532d0910acdc97072063c3a3e6e4a75b874
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-04-28 11:41:02 -0300

    Update des032.py

--- m1/pratica/desafios/des032.py
+++ m1/pratica/desafios/des032.py
@@ -1,9 +1,17 @@
-a = int(input('Em que ano estamos?... '))
+#Crie um programa que:
+#Leia um ano qualquer.
+#Mostre se ele é bissexto.
+
+a = int(input("Em que ano estamos?... "))
 cb = a % 4
 cb1 = a % 100
 cb2 = a % 400
 
 if cb == 0 and cb1 != 0 or cb2 == 0:
-    print(f'''BISSEXTO''')
+    print(f"""
+    BISSEXTO
+    """)
 else:
-    print(f'''NAO BISSEXTO''')
+    print(f"""
+    NÃO BISSEXTO
+    """)
